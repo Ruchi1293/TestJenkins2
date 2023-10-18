dda44073b346b252bed7f4cdd2aeec05a9f8d162
ruchi shrivastava <ruchishrivastava852@gmail.com>
2023-10-18 16:33:51 +0530

    added tests

--- JenkinsTest/UnitTest1.cs
+++ JenkinsTest/UnitTest1.cs
@@ -42,5 +42,23 @@ namespace JenkinsTest
             CreateAccount.Click();
             driver.Dispose();
         }
+        [Test]
+        public void Test4()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            IWebElement CreateAccount = driver.FindElement(By.XPath("//a[text()='Create new account']"));
+            CreateAccount.Click();
+            driver.Dispose();
+        }
+        [Test]
+        public void Test5()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            IWebElement CreateAccount = driver.FindElement(By.XPath("//a[text()='Create new account']"));
+            CreateAccount.Click();
+            driver.Dispose();
+        }
     }
 }
