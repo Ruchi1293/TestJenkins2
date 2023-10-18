2d1ce51a309b487fc02c781e12b3fc8f9e262bf6
ruchi shrivastava <ruchishrivastava852@gmail.com>
2023-10-18 16:36:40 +0530

    added category

--- JenkinsTest/UnitTest1.cs
+++ JenkinsTest/UnitTest1.cs
@@ -16,15 +16,15 @@ namespace JenkinsTest
         [Category("smoke")]
         public void Test1()
         {
-            //IWebDriver driver = new ChromeDriver();
-            //driver.Navigate().GoToUrl("https://www.facebook.com/");
-            //IWebElement CreateAccount = driver.FindElement(By.XPath("//a[text()='Create new account']"));
-            //CreateAccount.Click();
-            //driver.Dispose();
-            JObject q = JObject.Parse(File.ReadAllText("test.JSON"));
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            IWebElement CreateAccount = driver.FindElement(By.XPath("//a[text()='Create new account']"));
+            CreateAccount.Click();
+            driver.Dispose();
         }
 
         [Test]
+        [Category("smoke")]
         public void Test2()
         {
             IWebDriver driver = new ChromeDriver();
